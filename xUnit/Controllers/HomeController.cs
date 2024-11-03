229e7c5d018b3366860d4a5bc30745dd6edf3876
Nikhil Uprety <nikhil.uprety@crystalsolution.com.np>
2024-11-03 23:38:01 +0545

    Add project files.

---
+++ xUnit/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace xUnit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
