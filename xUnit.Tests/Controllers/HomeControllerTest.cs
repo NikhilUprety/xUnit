6be0cc8000e745dd6b9fa3b5abea60b07453755b
Nikhil Uprety <nikhil.uprety@crystalsolution.com.np>
2024-11-03 23:51:56 +0545

    .

---
+++ xUnit.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnit.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        public void HomeController_Index_ValidResult () 
+        {
+        
+        
+        }
+
+    }
+}
