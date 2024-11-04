ca90aca58c17b9eeb6339580e029c53641336843
Nikhil Uprety <nikhil.uprety@crystalsolution.com.np>
2024-11-04 13:30:53 +0545

    Test

--- xUnit.Tests/Controllers/HomeControllerTest.cs
+++ xUnit.Tests/Controllers/HomeControllerTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,27 +14,124 @@ namespace xUnit.Tests.Controllers
 {
     public class HomeControllerTest
     {
-   
+    
+      private readonly HomeController _controller=null;
+      private readonly Mock<IEmailService> _emailService =new Mock<IEmailService>();
+      private readonly Mock<IPrinterService> _printerService =new Mock<IPrinterService>();
+
+        public HomeControllerTest()
+        {
+            _controller=new HomeController(_emailService.Object, _printerService.Object);
+        }
 
         [Fact]
         public void HomeController_Index_ValidLargeNumberResult() 
         {
+        //Arrange
         
-         IPrinterService printerService = new PrinterService();       
-         IEmailService emailService = new EmailService();       
+         _printerService.Setup(x=>x.IsPrintAvailable()).Returns(true);     
+         _emailService.Setup(x => x.EmailExists()).Returns(true);
 
-         HomeController controller = new HomeController(emailService,printerService);
+   
          int guessedNumber = 122;
          string expectedResult = "wrong ! Try a smaller number";
-
-         string result = controller.Index(guessedNumber);
+ 
+        //Act
+         string result = _controller.Index(guessedNumber);
 
          Assert.Equal(result,expectedResult);
 
+        }
+
+        [Fact]
+        public void HomeController_Index_ValidLargeNumberResultWithoutEmailService()
+        {
+            //Arrange
+
+            _printerService.Setup(x => x.IsPrintAvailable()).Returns(true);
+            _emailService.Setup(x => x.EmailExists()).Returns(false);
 
 
+            int guessedNumber = 122;
+            string expectedResult = "wrong ! Try a smaller number";
+
+            string result = _controller.Index(guessedNumber);
+
+            Assert.Equal(result, expectedResult);
 
         }
 
+        [Fact]
+        public void HomeController_Index_ValidSmallNumberResult()
+        {
+            //Arrange
+
+            _printerService.Setup(x => x.IsPrintAvailable()).Returns(true);
+            _emailService.Setup(x => x.EmailExists()).Returns(true);
+
+
+            int guessedNumber =21 ;
+            string expectedResult = "wrong ! Try a bigger number";
+
+            string result = _controller.Index(guessedNumber);
+
+            Assert.Equal(result, expectedResult);
+
+        }
+
+        [Fact]
+        public void HomeController_Index_ValidSmallNumberResultWithoutEmailService()
+        {
+            //Arrange
+
+            _printerService.Setup(x => x.IsPrintAvailable()).Returns(true);
+            _emailService.Setup(x => x.EmailExists()).Returns(false);
+
+
+            int guessedNumber = 1;
+            string expectedResult = "wrong ! Try a bigger number";
+
+            string result = _controller.Index(guessedNumber);
+
+            Assert.Equal(result, expectedResult);
+
+        }
+
+        [Fact]
+        public void HomeController_Index_ValidNumberResult()
+        {
+            //Arrange
+
+            _printerService.Setup(x => x.IsPrintAvailable()).Returns(true);
+            _emailService.Setup(x => x.EmailExists()).Returns(true);
+
+
+            int guessedNumber = 100;
+            string expectedResult = "correct ! you guessed the number";
+
+            string result = _controller.Index(guessedNumber);
+
+            Assert.Equal(result, expectedResult);
+
+        }
+
+
+        [Fact]
+        public void HomeController_Index_ValidNumberResultWithoutEmailService()
+        {
+            //Arrange
+
+            _printerService.Setup(x => x.IsPrintAvailable()).Returns(true);
+            _emailService.Setup(x => x.EmailExists()).Returns(false);
+
+
+            int guessedNumber = 100;
+            string expectedResult = "correct ! you guessed the number";
+
+            string result = _controller.Index(guessedNumber);
+
+            Assert.Equal(result, expectedResult);
+
+        }
     }
 }
